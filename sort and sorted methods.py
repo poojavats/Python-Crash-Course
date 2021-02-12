06d7eaceaaa84e6495616902268b93b68a0e3d53
Pooja Sharma <35574750+poojavats@users.noreply.github.com>
2021-02-12 17:53:24 +0530

    Add files via upload

---
+++ sort and sorted methods.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### sort() method is used when we want to sort the data permantley and sorted() method is used for temporary. 
+
+# In[1]:
+
+
+cars=['bmw','audi','toyota','subaru']
+cars.sort()
+print(cars)
+
+
+# In[2]:
+
+
+cars.sort(reverse=True)
+print(cars)
+
+
+# In[3]:
+
+
+cars
+
+
+# In[4]:
+
+
+print(sorted(cars))
+
+
+# In[5]:
+
+
+print("\n here original iste{}".format (cars))
+
+
+# In[6]:
+
+
+world_places= ['Kailasha','Ireland','USA','Europe','Austraila']
+
+
+# In[7]:
+
+
+print(world_places)
+
+
+# In[8]:
+
+
+print(sorted(world_places))
+
+
+# In[9]:
+
+
+print(world_places)
+
+
+# In[13]:
+
+
+print(sorted(world_places,reverse=True))
+
+
+# In[15]:
+
+
+len(world_places)
+
+
+# In[ ]:
+
+
+
+
