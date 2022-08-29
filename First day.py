24764b1712133bb4f5e48d98c93344b4ed387010
Pooja Sharma <35574750+poojavats@users.noreply.github.com>
2022-08-29 23:11:30 +0100

    Add files via upload

---
+++ First day.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import this
+
+
+# # Shift+Tab-> used to get documentation
+
+# In[2]:
+
+
+print("Pooja")
+
+
+# In[3]:
+
+
+list()
+
+
+# In[4]:
+
+
+print ("this is my first program")
+
+
+# ###  We can use three lines of quotation marks to use multiple lines.
+
+# In[5]:
+
+
+#hi
+"""he
+pjbjs
+bxjdank"""
+
+
+# ### To check Data Types we use inbulit function type
+
+# In[6]:
+
+
+a=20
+
+
+# In[7]:
+
+
+type(a)
+
+
+# In[8]:
+
+
+b="Hello"
+
+
+# In[9]:
+
+
+type(b)
+
+
+# In[10]:
+
+
+b
+
+
+# In[11]:
+
+
+print(a,b,"then bye") 
+
+
+# In[12]:
+
+
+a,b= "Hello", 50
+
+
+# In[13]:
+
+
+a
+
+
+# In[14]:
+
+
+b
+
+
+# ### if we call both variables together, the output will be in tuple
+
+# In[15]:
+
+
+a,b
+
+
+# In[16]:
+
+
+type(a)
+
+
+# In[17]:
+
+
+type(b)
+
+
+# Note-> By using Tab button we can get hint also by typing any function
+
+# ## Names of variables shouldn't begin with integers.
+
+# In[18]:
+
+
+p=1.5
+
+
+# In[19]:
+
+
+type(p)
+
+
+# In[20]:
+
+
+p=5.8
+
+
+# In[21]:
+
+
+type(p)
+
+
+# In[23]:
+
+
+b= True
+c=False
+
+
+# In[24]:
+
+
+type(b)
+
+
+# ##  Value of True is 1 and False 0
+
+# In[29]:
+
+
+a= True+True+False+True
+
+
+# In[30]:
+
+
+a
+
+
+# In[31]:
+
+
+b= False+True+True-False
+
+
+# In[32]:
+
+
+b
+
+
+# In[33]:
+
+
+False-True+True
+
+
+# In[35]:
+
+
+print(True+True+True+False+True)
+
+
+# In[36]:
+
+
+True/False
+
+
+# ## Complex Numbers
+# ## if we use real with varible then it provides the real values and by using Imag it will provide Imaginary values
+
+# In[37]:
+
+
+n=45+6j
+
+
+# In[38]:
+
+
+type(n)
+
+
+# In[39]:
+
+
+n.real
+
+
+# In[41]:
+
+
+n.imag
+
+
+# In[ ]:
+
+
+
+
