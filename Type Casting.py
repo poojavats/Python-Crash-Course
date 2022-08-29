80e6a1d75a342c9c113b3a5ba52be8a1762cf545
Pooja Sharma <35574750+poojavats@users.noreply.github.com>
2022-08-30 00:26:17 +0100

    Add files via upload

---
+++ Type Casting.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Python is dynamic typing Programming language
+
+# ### Type Casting-> Conversion of one type of Data variable in to another type of data Variable is called Type Casting
+
+# In[4]:
+
+
+a= 1+"Pooja"
+
+
+# In[5]:
+
+
+a= str(1)+"Pooja"
+
+
+# In[6]:
+
+
+a
+
+
+# In[7]:
+
+
+"1"+"pooja"
+
+
+# In[8]:
+
+
+"pooja"*5
+
+
+# In[9]:
+
+
+a= 10
+
+
+# In[10]:
+
+
+a= input()
+
+
+# In[11]:
+
+
+a
+
+
+# In[13]:
+
+
+a= input("Enter a Integer ")
+b=input("Enter a Number ")
+c=input("Enter a  Integer ")
+
+
+# In[20]:
+
+
+k= int(a+b)/int(c)
+
+
+# In[21]:
+
+
+k
+
+
+# In[24]:
+
+
+o= k+6+2j
+o
+
+
+# In[26]:
+
+
+ooo= str(o)+"pooja"
+
+
+# In[27]:
+
+
+ooo
+
+
+# In[ ]:
+
+
+
+
