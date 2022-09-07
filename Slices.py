3134f4723bfe9c24d6c2db0aa193c1837f2c6043
Pooja Sharma <35574750+poojavats@users.noreply.github.com>
2022-09-07 15:45:59 +0100

    Add files via upload

---
+++ Slices.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+s="My Name is Pooja"
+s[5]
+
+
+# In[2]:
+
+
+s[3:6] 
+
+
+# In[3]:
+
+
+s[2:6:2]
+
+
+# In[4]:
+
+
+a="I am Pooja"
+
+
+# In[5]:
+
+
+a[0:3]
+
+
+# In[6]:
+
+
+a[1:2]
+
+
+# In[7]:
+
+
+a[1:5]
+
+
+# In[8]:
+
+
+a[2:8:1]
+
+
+# In[9]:
+
+
+a[2:9:2]
+
+
+# In[10]:
+
+
+s
+
+
+# In[12]:
+
+
+s[15:3:-2]
+#aops ea
+
+
+# In[14]:
+
+
+a[::-1]
+
+
+# In[15]:
+
+
+a="How are you all these days"
+
+
+# In[17]:
+
+
+a[2:-10]
+#w are you all
+
+
+# In[18]:
+
+
+a="Nike is a brand"
+
+
+# In[19]:
+
+
+a[::]
+
+
+# In[20]:
+
+
+a[::-1]
+
+
+# In[21]:
+
+
+a[1::]
+
+
+# In[22]:
+
+
+a[1:]
+
+
+# In[23]:
+
+
+for i in a:
+    print(i)
+
+
+# In[ ]:
+
+
+
+
